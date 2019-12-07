09aa1a1bf4c8dc40232d56024759b12c883926f3
Michel Tremblay <Micheltremblay91@gmail.com>
2019-12-06 19:33:08 -0500

    initial commit

--- index.js
+++ index.js
@@ -2,24 +2,53 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
+var frinkiac = require('frinkiac');
 
 bot.login(TOKEN);
 
+/**
+ * On bot ready
+ * 
+ * log the Discord id of the user
+ * 
+ * @return {String} Discord user id confirmation
+ */
 bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`);
 });
 
+/**
+ * On new message
+ * 
+ * if it starts with !homer, get the message and look up the quote
+ * 
+ * @params {String} msg the message
+ * 
+ * @return {String} URL to image result
+ */
 bot.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
-        msg.channel.send('pong');
-
-    } else if (msg.content.startsWith('!kick')) {
-        if (msg.mentions.users.size) {
-            const taggedUser = msg.mentions.users.first();
-            msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+  if (msg.content.startsWith('!homer')) {
+    let quote = msg.content.split('!homer');
+    quote = quote.filter((quote) => { return quote !== ''; });
+    frinkiac.search(quote)
+    .then(function(res) {
+        if (res.status !== 200) {
+            throw res;
         } else {
-            msg.reply('Please tag a valid user!');
+            return res.data;
         }
-    }
+    })
+    .then(function(data) {
+        var memeURLs = data.map(frinkiac.memeMap, frinkiac);
+        if (Array.isArray(memeURLs)) {
+          url = memeURLs[memeURLs.length];
+          msg.channel.send(url);
+        } else {
+          msg.channel.send(memeURLs);
+        }
+    })
+    .catch(function(err) {
+        throw err;
+    });
+  }
 });
