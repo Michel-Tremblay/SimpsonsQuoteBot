7a7f447dc7128d14340de61688d7d2046742fbe7
Michel Tremblay <Micheltremblay91@gmail.com>
2019-12-07 18:57:16 -0500

    sorted out how i want these promises to chain. now to figure out how to format a correct link

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const superagent = require('superagent');
+const Promise = require('bluebird');
+const rp = require('request-promise');
 const bot = new Discord.Client();
+const process = require('process');
 const TOKEN = process.env.TOKEN;
-var frinkiac = require('frinkiac');
+const frinkiac = require('frinkiac');
+const DEBUG_MODE = process.env.DEBUG;
 
+console.info('Starting... :)');
+
+/**
+ * login using the token in the .env file 
+ */
 bot.login(TOKEN);
 
 /**
@@ -26,25 +34,68 @@ bot.on('ready', () => {
  * @params {String} msg the message
  * 
  * @return {String} URL to image result
+ * 
+ * @TODO: hollow this out so that it's just a router to individual features 
+ * based on the command used to invoke it
  */
 bot.on('message', msg => {
+  /* if (DEBUG_MODE && msg.author.username !== 'Selleal') {
+    msg.channel.send('bot is currently under construction');
+  } */
   if (msg.content.startsWith('!homer')) {
     let quote = msg.content.split('!homer');
-    quote = quote.filter((quote) => { return quote !== ''; });
+    quote = quote.filter(quote => { return quote !== ''; });
     quote = quote[0].trim();
     let url = frinkiac.searchURL(quote);
-    (async () => {
-      try {
-        const res = await superagent.get(url);
-        memes = res.body;
-        memeUrls = [];
-        memes.forEach(element => {
-          memeUrls.push('https://frinkiac.com/caption/' + element.Episode + '/' + element.Timestamp);
+    let options = {
+      uri: url,
+      headers: {
+          'User-Agent': 'Request-Promise'
+      },
+      json: true
+  };
+    rp(options).then(results => {
+      let memes = [];
+      results.forEach(result => {
+        memes.push(frinkiac.captionURL(result.Episode, result.Timestamp));
+      });
+      return Promise.map(memes, obj => {
+        return rp(obj).then(body => {
+          return JSON.parse(body);
+        }).catch(reason => {
+          console.debug(reason);
         });
-        msg.author.send(`${memeUrls.length} results found \n ${memeUrls.join('\n')}`);
-      } catch (err) {
-        console.error(err);
-      }
-    })()
+      });
+    }).then(memes => {
+      return new Promise((resolve, reject) => {
+        let reply = ``;
+        let knownEpisodes = [];
+        if (!memes) {
+          return reject('Could not resolve memes');
+        }
+        /*
+          probably should not count punctuation and maybe some tolerance to 
+          whitespace
+        */
+        memes.forEach(meme => {
+          meme.Subtitles.forEach(subtitle => {
+            if (
+              subtitle.Content == quote &&
+              !knownEpisodes.includes(subtitle.Episode)
+            ) {
+              knownEpisodes.push(subtitle.Episode);
+              reply += `${frinkiac.memeURL(subtitle.Episode, subtitle.Timestamp,subtitle.Content)}`;
+            }
+          });
+        });
+         return resolve(reply); 
+      });
+    }).then(memes => {
+      msg.author.send(memes);
+    }).catch(reason => {
+      console.debug(reason);
+    });
   }
 });
+
+
