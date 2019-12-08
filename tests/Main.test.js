b1306e793309b05a1e84f839eea4c48e8e84d097
Michel Tremblay <Micheltremblay91@gmail.com>
2019-12-08 17:10:18 -0500

    ensuring test is done with json encoded message

--- tests/Main.test.js
+++ tests/Main.test.js
@@ -3,6 +3,6 @@ const fs = require("fs");
 
 it ('Should return meme link', async() => {
   fs.readFile('./samples/SampleDiscordMessage.json', async (err, data) => {
-    await expect(router.routeMsg(data)).resolves.toEqual('https://frinkiac.com/meme/S09E17/942924?b64lines=V0hPQS4uLiBUSU1FIEhBUyBSQVZBR0VEIFlPVVIgT05DRS1ZT1VUSEZVTCBMT09LUy4%3D');
+    await expect(router.routeMsg(JSON.parse(data))).resolves.toEqual('https://frinkiac.com/meme/S09E17/942924?b64lines=V0hPQS4uLiBUSU1FIEhBUyBSQVZBR0VEIFlPVVIgT05DRS1ZT1VUSEZVTCBMT09LUy4%3D');
   });
 });
