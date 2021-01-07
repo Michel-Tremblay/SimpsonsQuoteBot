91a53bc5782328acdabd556bd977a81893e5462b
Michel Tremblay <Micheltremblay91@gmail.com>
2021-01-07 15:09:23 -0500

    wrap in try catch

--- tests/Main.test.js
+++ tests/Main.test.js
@@ -2,11 +2,14 @@ const fs = require('fs');
 const router = require('../index');
 
 it('Should return meme link', async () => {
-  fs.readFile('./samples/SampleDiscordMessage.json', async (err, data) => {
-    try {
+  try {
+    fs.readFile('./samples/SampleDiscordMessage.json', async (err, data) => {
+      if (err) {
+        return err;
+      }
       await expect(router.routeMsg(JSON.parse(data))).resolves.toEqual('https://frinkiac.com/meme/S09E17/942924?b64lines=V0hPQS4uLiBUSU1FIEhBUyBSQVZBR0VEIFlPVVIgT05DRS1ZT1VUSEZVTCBMT09LUy4%3D');
-    } catch (e) {
-      return e;
-    }
-  });
+    });
+  } catch (e) {
+    return e;
+  }
 });
