4d88e0042299e2654186ee7a8948b0359bdb4ea7
Michel Tremblay <Micheltremblay91@gmail.com>
2021-01-07 21:38:23 -0500

    introduced invalid flag test with message template

--- tests/Main.test.js
+++ tests/Main.test.js
@@ -6,6 +6,7 @@ const help = require('../Features/Help');
 const memeMessageJson = 'samples/MemeMessage.json';
 const helpMessageJson = 'samples/HelpMessage.json';
 const defaultMessageJson = 'samples/NoFlagMessage.json';
+const InvalidFlagMessageJson = 'samples/InvalidFlagMessage.json';
 
 // eslint-disable-next-line consistent-return
 it('Should return meme link', () => {
@@ -28,6 +29,15 @@ it('Should return help message', () => {
 // eslint-disable-next-line consistent-return
 it('Should return help message (no flag)', () => {
   const jsonData = fs.readFileSync(path.join(__dirname, defaultMessageJson), 'utf8');
+  const message = JSON.parse(jsonData);
+  router.routeMsg(message).then((msg) => {
+    expect(msg).toEqual(help.responsePage);
+  }).catch((error) => error);
+});
+
+// eslint-disable-next-line consistent-return
+it('Should return help message (invalid flag)', () => {
+  const jsonData = fs.readFileSync(path.join(__dirname, InvalidFlagMessageJson), 'utf8');
   const message = JSON.parse(jsonData);
   router.routeMsg(message).then((msg) => {
     expect(msg).toEqual(help.responsePage);
